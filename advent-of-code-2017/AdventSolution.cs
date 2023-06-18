bcadbcb9afd77a9eb4ceecd62d9fab570932291a
Bret Gourdie <BretGourdie@gmail.com>
2023-06-18 01:26:18 -0400

    Solved Day 01

---
+++ advent-of-code-2017/AdventSolution.cs
@@ -0,0 +1,133 @@
+namespace advent_of_code_2017
+{
+    [TestFixture]
+    public abstract class AdventSolution
+    {
+        private long _expected;
+        private long _actual;
+
+        private long _exampleExpected;
+        private long _inputExpected;
+
+        private bool _wasExpectedMethodCalled;
+
+        [SetUp]
+        public void Setup()
+        {
+            fileCheckAssertions();
+        }
+
+        private void fileCheckAssertions()
+        {
+            Assert.That(File.Exists(getFilename(InputType.Example)), "Example file does not exist");
+            Assert.That(File.Exists(getFilename(InputType.Input)), "Input file does not exist");
+        }
+
+        [Test]
+        [TestCase(InputType.Example)]
+        [TestCase(InputType.Input)]
+        public void Part1(InputType inputType) =>
+            Assert.That(() =>
+            part(
+                inputType,
+                part1Work,
+                part1ExampleExpected,
+                part1InputExpected),
+            Throws.Nothing);
+
+
+        [Test]
+        [TestCase(InputType.Example)]
+        [TestCase(InputType.Input)]
+        public void Part2(InputType inputType) =>
+            Assert.That(() =>
+            part(
+                inputType,
+                part2Work,
+                part2ExampleExpected,
+                part2InputExpected),
+            Throws.Nothing);
+
+        protected abstract long part1Work(string[] input);
+        protected void part1Expected(
+            long exampleExpected,
+            long inputExpected)
+        {
+
+        }
+
+        protected void part2Expected(
+            long exampleExpected,
+            long inputExpected)
+        {
+
+        }
+
+        private void expected(
+            long exampleExpected,
+            long inputExpected)
+        {
+            _exampleExpected = exampleExpected;
+            _inputExpected = inputExpected;
+            _wasExpectedMethodCalled = true;
+        }
+
+        protected abstract long part1ExampleExpected { get; }
+        protected abstract long part1InputExpected { get; }
+
+        protected abstract long part2Work(string[] input);
+        protected abstract long part2ExampleExpected { get; }
+        protected abstract long part2InputExpected { get; }
+
+
+
+        private void part(
+            InputType type,
+            Func<string[], long> workMethod,
+            long exampleExpected,
+            long inputExpected)
+        {
+            var answer = workMethod.Invoke(getInput(type));
+
+            switch (type)
+            {
+                case InputType.Example:
+                    assertions(exampleExpected, answer);
+                    break;
+                case InputType.Input:
+                    assertions(inputExpected, answer);
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+
+        private void assertions(
+            long expected,
+            long actual)
+        {
+            _expected = expected;
+            _actual = actual;
+
+            Assert.That(expected == actual, $"Expected {expected}; actual {actual}");
+        }
+
+        private string[] getInput(InputType inputType)
+        {
+            return File.ReadAllLines(
+                getFilename(inputType)
+            );
+        }
+
+        private string getFilename(InputType inputType)
+        {
+            return
+                this.GetType().Name
+                + Path.DirectorySeparatorChar
+                + inputType.ToString()
+                + ".txt";
+        }
+
+        public enum InputType { Example, Input };
+    }
+}
---
+++ advent-of-code-2017/Day01/Day01.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace advent_of_code_2017.Day01
+{
+    internal class Day01 : AdventSolution
+    {
+        protected override long part1ExampleExpected => 3;
+
+        protected override long part1InputExpected => 1175;
+
+        protected override long part2ExampleExpected => 4;
+
+        protected override long part2InputExpected => 1166;
+
+        protected override long part1Work(string[] input)
+        {
+            var line = input[0];
+            var prev = -1;
+            long sum = 0;
+            for (int ii = 0; ii < line.Length; ii++)
+            {
+                var current = int.Parse(line[ii].ToString());
+
+                if (current == prev)
+                {
+                    sum += current;
+                }
+
+                prev = current;
+            }
+
+            if (line[0] == line[line.Length - 1])
+            {
+                sum += int.Parse(line[0].ToString());
+            }
+
+            return sum;
+        }
+
+        protected override long part2Work(string[] input)
+        {
+            var line = input[0];
+            var around = -1;
+            long sum = 0;
+
+            for (int ii = 0; ii < line.Length; ii++)
+            {
+                var current = int.Parse(line[ii].ToString());
+                var aroundIndex = (ii + line.Length / 2) % line.Length;
+                around = int.Parse(line[aroundIndex].ToString());
+
+                if (current == around)
+                {
+                    sum += current;
+                }
+            }
+
+            return sum;
+        }
+    }
+}
