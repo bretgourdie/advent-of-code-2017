586ac33bf1ce4a547869f5773b12d1b628eaef68
Bret Gourdie <BretGourdie@gmail.com>
2023-06-20 01:02:43 -0400

    Start work on Day03

---
+++ advent-of-code-2017/Day03/Day03.cs
@@ -0,0 +1,23 @@
+﻿namespace advent_of_code_2017.Day03
+{
+    internal class Day03 : AdventSolution
+    {
+        protected override long part1ExampleExpected => 31;
+
+        protected override long part1InputExpected => 419;
+
+        protected override long part2ExampleExpected => throw new NotImplementedException();
+
+        protected override long part2InputExpected => throw new NotImplementedException();
+
+        protected override long part1Work(string[] input)
+        {
+            return new Grid().Generate(int.Parse(input.First()), x => x + 1);
+        }
+
+        protected override long part2Work(string[] input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ advent-of-code-2017/Day03/Grid.cs
@@ -0,0 +1,38 @@
+﻿namespace advent_of_code_2017.Day03
+{
+    internal class Grid
+    {
+        public long Generate(int target, Func<int, int> getNext)
+        {
+            IDictionary<Point2D, int> grid = new Dictionary<Point2D, int>();
+
+            int x = 0;
+            int y = 0;
+            int current = 1;
+            int moves = 0;
+
+            while (current != target)
+            {
+                current = getNext(current);
+
+                x = goX(moves, current);
+                y = goY(moves, current);
+
+                var point = new Point2D(x, y);
+                grid[point] = current;
+            }
+
+            throw new NotImplementedException();
+        }
+
+        private int goX(int moves, int current)
+        {
+            throw new NotImplementedException();
+        }
+
+        private int goY(int moves, int current)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
