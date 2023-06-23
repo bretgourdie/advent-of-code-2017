8f7cb437bff1aba61030f18adae6ce2698c9a95b
Bret Gourdie <BretGourdie@gmail.com>
2023-06-23 13:17:23 -0400

    Add inputs; write solutions

---
+++ advent-of-code-2017/Day07/Day07.cs
@@ -0,0 +1,21 @@
+﻿namespace advent_of_code_2017.Day07;
+internal class Day07 : AdventSolutionTemplate<string, long>
+{
+    protected override string part1ExampleExpected => "tknk";
+
+    protected override string part1InputExpected => "rqwgj";
+
+    protected override long part2ExampleExpected => throw new NotImplementedException();
+
+    protected override long part2InputExpected => throw new NotImplementedException();
+
+    protected override string part1Work(string[] input)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override long part2Work(string[] input)
+    {
+        throw new NotImplementedException();
+    }
+}
