78449eb31963abb6c7b7a8cd1884540839a0bf6f
Bret Gourdie <BretGourdie@gmail.com>
2023-06-29 18:19:14 -0400

    Begin work on Day 08 Part 1
    Expression evaluator not working

---
+++ advent-of-code-2017/Day08/Day08.cs
@@ -0,0 +1,29 @@
+﻿namespace advent_of_code_2017.Day08;
+internal class Day08 : AdventSolution
+{
+    protected override long part1ExampleExpected => 1;
+
+    protected override long part1InputExpected => 3089;
+
+    protected override long part2ExampleExpected => throw new NotImplementedException();
+
+    protected override long part2InputExpected => throw new NotImplementedException();
+
+    protected override long part1Work(string[] input)
+    {
+        var registers = new Dictionary<string, long>();
+
+        foreach (var line in input)
+        {
+            var instruction = new Instruction(line);
+            instruction.Affect(registers);
+        }
+
+        return registers.Values.Max();
+    }
+
+    protected override long part2Work(string[] input)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ advent-of-code-2017/Day08/Instruction.cs
@@ -0,0 +1,78 @@
+﻿using System.Data;
+
+namespace advent_of_code_2017.Day08;
+internal class Instruction
+{
+    readonly string TargetRegister;
+    readonly TargetOperation TheTargetOperation;
+    readonly long Immediate;
+
+    readonly string If;
+    readonly string SourceRegister;
+    readonly string ComparisonOperator;
+    readonly long ComparisonImmediate;
+
+
+    public Instruction(string line)
+    {
+        var split = line.Split(' ');
+
+        TargetRegister = split[0];
+        TheTargetOperation = Enum.Parse<TargetOperation>(split[1]);
+        Immediate = long.Parse(split[2]);
+
+        If = split[3];
+        SourceRegister = split[4];
+        ComparisonOperator = split[5];
+        ComparisonImmediate = long.Parse(split[6]);
+    }
+
+    public void Affect(IDictionary<string, long> registers)
+    {
+        var conditionsWereMet = conditionMet(
+            getRegisterValue(registers, SourceRegister),
+            ComparisonOperator,
+            ComparisonImmediate);
+
+        if (conditionsWereMet)
+        {
+            var targetValue = getRegisterValue(registers, TargetRegister);
+            var toAdd = getChange(TheTargetOperation, Immediate);
+
+            registers[TargetRegister] = targetValue + toAdd;
+        }
+    }
+
+    private long getChange(TargetOperation targetOperation, long value)
+    {
+        switch (targetOperation)
+        {
+            case TargetOperation.inc: return value;
+            case TargetOperation.dec: return value * -1;
+            default:
+                throw new ArgumentException(nameof(TargetOperation));
+        }
+    }
+
+    private long getRegisterValue(IDictionary<string, long> registers, string register) =>
+        registers.ContainsKey(register)
+            ? registers[register]
+            : 0;
+
+    private bool conditionMet(
+        long value,
+        string comparisonOperator,
+        long comparisonImmediate)
+    {
+        var dt = new DataTable();
+        var expression = value.ToString() + comparisonOperator + comparisonImmediate.ToString();
+        bool answer = (bool)dt.Compute(expression, String.Empty);
+        return answer;
+    }
+
+    private enum TargetOperation
+    {
+        inc,
+        dec
+    }
+}
