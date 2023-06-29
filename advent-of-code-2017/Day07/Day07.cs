c23784b43b00544301a681036a1ec923f9aaf736
Bret Gourdie <BretGourdie@gmail.com>
2023-06-29 01:40:23 -0400

    Solve Day 07 Parts 1 and 2, mostly
    Part 2 input needs to be corrected

--- advent-of-code-2017/Day07/Day07.cs
+++ advent-of-code-2017/Day07/Day07.cs
@@ -5,17 +5,83 @@ internal class Day07 : AdventSolutionTemplate<string, long>
 
     protected override string part1InputExpected => "rqwgj";
 
-    protected override long part2ExampleExpected => throw new NotImplementedException();
+    protected override long part2ExampleExpected => 60;
 
-    protected override long part2InputExpected => throw new NotImplementedException();
+    protected override long part2InputExpected => 333;
 
-    protected override string part1Work(string[] input)
+    private Node getRoot(IDictionary<string, Node> nodeLookup)
     {
-        throw new NotImplementedException();
+        var nodes = nodeLookup.Keys;
+        var candidateNodes = new HashSet<string>(nodes);
+
+        foreach (var node in nodes)
+        {
+            if (nodeLookup.Values.Any(x => x.Children.Contains(node)))
+            {
+                candidateNodes.Remove(node);
+            }
+        }
+
+        return nodeLookup[candidateNodes.Single()];
     }
+
+    private IDictionary<string, Node> getNodeLookup(string[] input)
+    {
+        var dict = new Dictionary<string, Node>();
+
+        foreach (var element in input)
+        {
+            var node = new Node(element);
+
+            dict[node.Name] = node;
+        }
+
+        return dict;
+    }
+
+    protected override string part1Work(string[] input) =>
+        getRoot(getNodeLookup(input)).Name;
 
     protected override long part2Work(string[] input)
     {
-        throw new NotImplementedException();
+        var nodeLookup = getNodeLookup(input);
+        var root = getRoot(nodeLookup);
+
+        var balanceNeeded = determineBalancing(root, nodeLookup);
+
+        return balanceNeeded;
+    }
+
+    private long determineBalancing(
+        Node root,
+        IDictionary<string, Node> nodeLookup)
+    {
+        var nodeToChildrenWeight = new Dictionary<string, long>();
+        foreach (var child in root.Children)
+        {
+            var weight = determineBalancing(nodeLookup[child], nodeLookup);
+            nodeToChildrenWeight[child] = weight;
+        }
+
+        long weightToBe = -1;
+        if (nodeToChildrenWeight.Values.Distinct().Count() > 1)
+        {
+            var oddOneOut = nodeToChildrenWeight
+                .Where(x => nodeToChildrenWeight.Count(y => y.Value == x.Value) == 1)
+                .Single();
+
+            var otherWeight = nodeToChildrenWeight.Values
+                .Where(x => nodeToChildrenWeight.Values.Count(y => y == x) > 1)
+                .Distinct()
+                .Single();
+
+            return nodeLookup[oddOneOut.Key].Weight
+                + (otherWeight - oddOneOut.Value);
+        }
+
+        else
+        {
+            return nodeToChildrenWeight.Values.Sum() + root.Weight;
+        }
     }
 }
---
+++ advent-of-code-2017/Day07/Node.cs
@@ -0,0 +1,28 @@
+﻿namespace advent_of_code_2017.Day07;
+internal class Node
+{
+    public readonly IEnumerable<string> Children;
+    public readonly string Name;
+    public readonly long Weight;
+
+    public Node(string line)
+    {
+        var arrowSplit = line.Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+
+        var nameAndWeight = arrowSplit[0].Split(' ');
+        this.Name = nameAndWeight[0];
+        this.Weight = long.Parse(nameAndWeight[1].Replace("(", "").Replace(")", ""));
+
+        if (arrowSplit.Length >= 2)
+        {
+            var childrenSplit = arrowSplit[1].Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            this.Children = childrenSplit;
+        }
+
+        else
+        {
+            Children = Enumerable.Empty<string>();
+        }
+    }
+}
