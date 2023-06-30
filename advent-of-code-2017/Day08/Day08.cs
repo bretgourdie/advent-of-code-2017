03d12468eed128f5e8764a4c4bf9a745b3abfb5a
Bret Gourdie <BretGourdie@gmail.com>
2023-06-29 23:25:39 -0400

    Solve Day 8 Part 2

--- advent-of-code-2017/Day08/Day08.cs
+++ advent-of-code-2017/Day08/Day08.cs
@@ -5,25 +5,47 @@ internal class Day08 : AdventSolution
 
     protected override long part1InputExpected => 3089;
 
-    protected override long part2ExampleExpected => throw new NotImplementedException();
+    protected override long part2ExampleExpected => 10;
 
-    protected override long part2InputExpected => throw new NotImplementedException();
+    protected override long part2InputExpected => 5391;
 
-    protected override long part1Work(string[] input)
+    private long work(
+        string[] input,
+        Func<long, long, long> takeStrategy)
     {
         var registers = new Dictionary<string, long>();
+        long maxAtAnyPoint = -1;
 
         foreach (var line in input)
         {
             var instruction = new Instruction(line);
             instruction.Affect(registers);
+            maxAtAnyPoint = Math.Max(maxAtAnyPoint, getMaxRegister(registers));
         }
 
-        return registers.Values.Max();
+        return takeStrategy(getMaxRegister(registers), maxAtAnyPoint);
     }
 
-    protected override long part2Work(string[] input)
+    private long getMaxRegister(IDictionary<string, long> registers)
     {
-        throw new NotImplementedException();
+        if (registers.Any())
+        {
+            return registers.Values.Max();
+        }
+
+        else
+        {
+            return -1;
+        }
     }
+
+    private long takeMaxAtEnd(long maxAtEnd, long maxAtAnyPoint) => maxAtEnd;
+
+    private long takeMaxAtAnyPoint(long maxAtEnd, long maxAtAnyPoint) => maxAtAnyPoint;
+
+    protected override long part1Work(string[] input) =>
+        work(input, takeMaxAtEnd);
+
+    protected override long part2Work(string[] input) =>
+        work(input, takeMaxAtAnyPoint);
 }
