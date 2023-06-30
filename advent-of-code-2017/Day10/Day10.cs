7c5749d5f795f0acb2a806b697a76f749de1e829
Bret Gourdie <BretGourdie@gmail.com>
2023-06-30 13:25:01 -0400

    Solved Day 10 Part 1

---
+++ advent-of-code-2017/Day10/Day10.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace advent_of_code_2017.Day10;
+internal class Day10 : AdventSolutionTemplate<long, string>
+{
+    protected override long part1ExampleExpected => 12;
+
+    protected override long part1InputExpected => 19591;
+
+    protected override string part2ExampleExpected => throw new NotImplementedException();
+
+    protected override string part2InputExpected => "62e2204d2ca4f4924f6e7a80f1288786";
+
+    protected override long part1Work(string[] input)
+    {
+        var line = input.Single();
+
+        int length;
+        if (line.Count(x => x == ',') + 1 <= 4)
+            length = 5;
+        else
+            length = 256;
+
+        return new KnotHash().Hash(line, length);
+    }
+
+    protected override string part2Work(string[] input)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ advent-of-code-2017/Day10/KnotHash.cs
@@ -0,0 +1,58 @@
+﻿namespace advent_of_code_2017.Day10;
+internal class KnotHash
+{
+    public long Hash(string line, int numbersLength)
+    {
+        return
+            Hash(
+                numbersLength,
+                line
+                    .Split(",", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(x => int.Parse(x))
+                    .ToList()
+            );
+
+    }
+
+    public long Hash(IList<int> lengths) => Hash(256, lengths);
+
+    public long Hash(int numbersLength, IList<int> lengths)
+    {
+        var numbers = startingNumbers(numbersLength);
+        var currentIndex = 0;
+
+        for (int skipSize = 0; skipSize < lengths.Count; skipSize++)
+        {
+            var lengthSize = lengths[skipSize];
+
+            var list = new List<int>();
+            for (int ii = 0; ii < lengthSize; ii++)
+            {
+                var sliceIndex = (currentIndex + ii) % numbers.Length;
+                list.Add(numbers[sliceIndex]);
+            }
+
+            list.Reverse();
+
+            for (int ii = 0; ii < lengthSize; ii++)
+            {
+                var putIndex = (currentIndex + ii) % numbers.Length;
+                numbers[putIndex] = list[ii];
+            }
+
+            currentIndex += lengthSize + skipSize;
+        }
+
+        return numbers[0] * numbers[1];
+    }
+
+    private int[] startingNumbers(int length) => startingNumberYields(length).ToArray();
+
+    private IEnumerable<int> startingNumberYields(int length)
+    {
+        for (int ii = 0; ii < length; ii++)
+        {
+            yield return ii;
+        }
+    }
+}
