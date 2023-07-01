a8a7bb26c7f2e688c1331c81a27e2efe2821bc03
Bret Gourdie <BretGourdie@gmail.com>
2023-07-01 02:32:03 -0400

    Solved Day 11 Parts 1 and 2

---
+++ advent-of-code-2017/Day11/Day11.cs
@@ -0,0 +1,26 @@
+﻿namespace advent_of_code_2017.Day11;
+internal class Day11 : AdventSolution
+{
+    protected override long part1ExampleExpected => 3;
+
+    protected override long part1InputExpected => 759;
+
+    protected override long part2ExampleExpected => 3;
+
+    protected override long part2InputExpected => 1501;
+
+    private long work(string[] input, Func<long, long, long> returnStrategy)
+    {
+        var instructions = input.Single().Split(',');
+
+        return new HexNavigation().Navigate(instructions, returnStrategy);
+    }
+
+    protected override long part1Work(string[] input) => work(input, takeEndingDistance);
+
+    protected override long part2Work(string[] input) => work(input, takeMaxDistance);
+
+    private long takeEndingDistance(long end, long max) => end;
+
+    private long takeMaxDistance(long end, long max) => max;
+}
---
+++ advent-of-code-2017/Day11/HexNavigation.cs
@@ -0,0 +1,55 @@
+﻿namespace advent_of_code_2017.Day11;
+internal class HexNavigation
+{
+    private readonly IDictionary<string, Navigation> instructionToNavigation =
+        new Dictionary<string, Navigation>()
+        {
+            {"n", new Navigation(+0, -1) },
+            {"ne", new Navigation(+1, -1) },
+            {"se", new Navigation(+1, +0) },
+            {"s", new Navigation(+0, +1) },
+            {"sw", new Navigation(-1, +1) },
+            {"nw", new Navigation(-1, +0) }
+        };
+
+    public long Navigate(
+        string[] instructions,
+        Func<long, long, long> returnStrategy)
+    {
+        long q = 0;
+        long r = 0;
+        long maxDistance = -1;
+
+        foreach (var instruction in instructions)
+        {
+            var navigation = instructionToNavigation[instruction];
+
+            q += navigation.Q;
+            r += navigation.R;
+
+            maxDistance = Math.Max(maxDistance, manhattanDistance(q, r));
+        }
+
+        return returnStrategy(manhattanDistance(q, r), maxDistance);
+    }
+
+    private long manhattanDistance(long q, long r)
+    {
+        return
+            (q > 0 ? q : 0)
+            + (r > 0 ? r : 0)
+            + (-q - r > 0 ? -q - r : 0);
+    }
+
+    private struct Navigation
+    {
+        public long Q;
+        public long R;
+
+        public Navigation(long q, long r)
+        {
+            Q = q;
+            R = r;
+        }
+    }
+}
