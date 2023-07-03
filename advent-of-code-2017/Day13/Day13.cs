8a4a88811f1ad103d614d15ffb166f13da90124a
Bret Gourdie <BretGourdie@gmail.com>
2023-07-03 01:19:04 -0400

    Solved Day 13 Part 1

---
+++ advent-of-code-2017/Day13/Day13.cs
@@ -0,0 +1,52 @@
+﻿namespace advent_of_code_2017.Day13;
+internal class Day13 : AdventSolution
+{
+    protected override long part1ExampleExpected => 24;
+
+    protected override long part1InputExpected => 1624;
+
+    protected override long part2ExampleExpected => throw new NotImplementedException();
+
+    protected override long part2InputExpected => throw new NotImplementedException();
+
+    protected override long part1Work(string[] input)
+    {
+        var scanners = new Dictionary<int, Scanner>();
+
+        foreach (var line in input)
+        {
+            var split = line.Split(": ");
+            var depth = int.Parse(split[0]);
+            var range = int.Parse(split[1]);
+            scanners.Add(depth, new Scanner(range));
+        }
+
+        int severity = 0;
+        var maxDepth = scanners.Keys.Max();
+
+        for (int depth = 0; depth <= maxDepth; depth++)
+        {
+            if (scanners.ContainsKey(depth) && scanners[depth].Detected)
+            {
+                severity += depth * scanners[depth].Range;
+            }
+
+            updateScanners(scanners);
+        }
+
+        return severity;
+    }
+
+    private void updateScanners(IDictionary<int, Scanner> scanners)
+    {
+        foreach (var scanner in scanners.Keys)
+        {
+            scanners[scanner].Update();
+        }
+    }
+
+    protected override long part2Work(string[] input)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ advent-of-code-2017/Day13/Scanner.cs
@@ -0,0 +1,24 @@
+﻿namespace advent_of_code_2017.Day13;
+internal class Scanner
+{
+    public int Index { get; private set; }
+    int direction = 1;
+
+    public readonly int Range;
+    public bool Detected { get { return Index == 0; } }
+
+    public Scanner(int range)
+    {
+        Range = range;
+    }
+ 
+    public void Update()
+    {
+        Index += direction;
+
+        if (Index == 0 || Index == Range - 1)
+        {
+            direction *= -1;
+        }
+    }
+}
