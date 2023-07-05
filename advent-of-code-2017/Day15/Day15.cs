db96dfff400c7a34c9e944dae9412bf6f043be06
Bret Gourdie <BretGourdie@gmail.com>
2023-07-05 01:37:34 -0400

    Solved Day 15 Part 1

---
+++ advent-of-code-2017/Day15/Day15.cs
@@ -0,0 +1,39 @@
+﻿namespace advent_of_code_2017.Day15;
+internal class Day15 : AdventSolution
+{
+    protected override long part1ExampleExpected => 588;
+
+    protected override long part1InputExpected => 594;
+
+    protected override long part2ExampleExpected => throw new NotImplementedException();
+
+    protected override long part2InputExpected => throw new NotImplementedException();
+
+    protected override long part1Work(string[] input)
+    {
+        var lower16Bits = 0b1111_1111_1111_1111;
+
+        var a = new Generator(input.First());
+        var b = new Generator(input.Skip(1).First());
+
+        long matches = 0;
+
+        for (int ii = 0; ii < 40_000_000; ii++)
+        {
+            var aValue = a.Generate();
+            var bValue = b.Generate();
+
+            if ((aValue & lower16Bits) == (bValue & lower16Bits))
+            {
+                matches += 1;
+            }
+        }
+
+        return matches;
+    }
+
+    protected override long part2Work(string[] input)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ advent-of-code-2017/Day15/Generator.cs
@@ -0,0 +1,36 @@
+﻿namespace advent_of_code_2017.Day15;
+internal class Generator
+{
+    private static readonly IDictionary<string, long> GeneratorToFactors =
+        new Dictionary<string, long>()
+        {
+            {"A", 16807},
+            {"B", 48271}
+        };
+
+    private readonly long factor;
+    private const long divisor = 2147483647;
+
+    private long lastValue;
+
+    public Generator(string line)
+    {
+        var split = line.Split(" starts with ");
+        var generator = split[0].Replace("Generator ", String.Empty);
+        factor = Generator.GeneratorToFactors[generator];
+
+        var startValue = long.Parse(split[1]);
+        lastValue = startValue;
+    }
+
+    public long Generate()
+    {
+        var next = lastValue * factor;
+
+        var remainder = next % divisor;
+
+        lastValue = remainder;
+
+        return remainder;
+    }
+}
