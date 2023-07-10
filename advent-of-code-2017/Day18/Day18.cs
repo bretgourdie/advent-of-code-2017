ad33c748076dc8e204c925d6efda2a9990972200
Bret Gourdie <BretGourdie@gmail.com>
2023-07-10 17:19:31 -0400

    Solved Day 18 Part 1

---
+++ advent-of-code-2017/Day18/Day18.cs
@@ -0,0 +1,21 @@
+﻿namespace advent_of_code_2017.Day18;
+internal class Day18 : AdventSolution
+{
+    protected override long part1Work(string[] input)
+    {
+        return new Duet().Play(input);
+    }
+
+    protected override long part1ExampleExpected => 4;
+
+    protected override long part1InputExpected => 7071;
+
+    protected override long part2Work(string[] input)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override long part2ExampleExpected => -1;
+
+    protected override long part2InputExpected => 8001;
+}
---
+++ advent-of-code-2017/Day18/Duet.cs
@@ -0,0 +1,134 @@
+﻿namespace advent_of_code_2017.Day18;
+internal class Duet
+{
+    public long Play(IList<string> instructions)
+    {
+        IDictionary<string, long> registers = new Dictionary<string, long>();
+
+        long lastPlayedSound = 0;
+
+        for (int ii = 0; ii < instructions.Count; ii++)
+        {
+            var instruction = instructions[ii];
+            var split = instruction.Split(' ');
+
+            var instructionCode = split[0];
+
+            switch (instructionCode)
+            {
+                case "snd":
+                    lastPlayedSound = sound(split[1], registers);
+                    break;
+                case "set":
+                    set(split[1], split[2], registers);
+                    break;
+                case "add":
+                    add(split[1], split[2], registers);
+                    break;
+                case "mul":
+                    multiply(split[1], split[2], registers);
+                    break;
+                case "mod":
+                    modulo(split[1], split[2], registers);
+                    break;
+                case "rcv":
+                    if (recover(split[1], registers))
+                    {
+                        return lastPlayedSound;
+                    }
+                    break;
+                case "jgz":
+                    long jumpAmount = jumpGreaterThanZero(split[1], split[2], registers);
+                    if (jumpAmount < 0)
+                    {
+                        ii += (int)jumpAmount - 1;
+                    }
+                    else
+                    {
+                        ii += (int)jumpAmount;
+                    }
+                    break;
+            }
+        }
+
+        throw new ArgumentException(nameof(instructions));
+    }
+
+    private long sound(
+        string argument,
+        IDictionary<string, long> registers)
+    {
+        return getValue(argument, registers);
+    }
+
+    private void set(
+        string arg1,
+        string arg2,
+        IDictionary<string, long> registers)
+    {
+        registers[arg1] = getValue(arg2, registers);
+    }
+
+    private void add(
+        string arg1,
+        string arg2,
+        IDictionary<string, long> registers)
+    {
+        registers[arg1] = getValue(arg1, registers) + getValue(arg2, registers);
+    }
+
+    private void multiply(
+        string arg1,
+        string arg2,
+        IDictionary<string, long> registers)
+    {
+        registers[arg1] = getValue(arg1, registers) * getValue(arg2, registers);
+    }
+
+    private void modulo(
+        string arg1,
+        string arg2,
+        IDictionary<string, long> registers)
+    {
+        registers[arg1] = getValue(arg1, registers) % getValue(arg2, registers);
+    }
+
+    private bool recover(
+        string arg1,
+        IDictionary<string, long> registers)
+    {
+        return getValue(arg1, registers) != 0;
+    }
+
+    private long jumpGreaterThanZero(
+        string arg1,
+        string arg2,
+        IDictionary<string, long> registers)
+    {
+        if (getValue(arg1, registers) > 0)
+        {
+            return getValue(arg2, registers);
+        }
+
+        else
+        {
+            return 0;
+        }
+    }
+
+    private long getValue(
+        string arg,
+        IDictionary<string, long> registers)
+    {
+        if (long.TryParse(arg, out long immediateValue))
+        {
+            return immediateValue;
+        }
+
+        else
+        {
+            registers.TryGetValue(arg, out long registerValue);
+            return registerValue;
+        }
+    }
+}
