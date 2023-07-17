38e247ef9b95ccd2ea037af03c9751a246349795
Bret Gourdie <BretGourdie@gmail.com>
2023-07-17 12:49:53 -0400

    Solved Day 19 Part 1

---
+++ advent-of-code-2017/Day19/Day19.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace advent_of_code_2017.Day19;
+internal class Day19 : AdventSolutionTemplate<string, long>
+{
+    protected override string part1Work(string[] input)
+    {
+        return new Packet().Traverse(input);
+    }
+
+    protected override string part1ExampleExpected => "ABCDEF";
+    protected override string part1InputExpected => "RYLONKEWB";
+    protected override long part2Work(string[] input)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override long part2ExampleExpected => 38;
+    protected override long part2InputExpected => 16016;
+}
---
+++ advent-of-code-2017/Day19/Packet.cs
@@ -0,0 +1,143 @@
+﻿using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace advent_of_code_2017.Day19;
+internal class Packet
+{
+    private const char traverseNS = '|';
+    private const char traverseEW = '-';
+    private const char traverseTurn = '+';
+    private const char traverseBlank = ' ';
+
+    private readonly IEnumerable<Direction> NorthSouth = new[]
+    {
+        Direction.North,
+        Direction.South
+    };
+
+    private readonly IEnumerable<Direction> EastWest = new[]
+    {
+        Direction.East,
+        Direction.West
+    };
+
+    public string Traverse(IList<string> map)
+    {
+        var letters = new StringBuilder();
+
+        Direction currentDirection = Direction.South;
+
+        int y = 0;
+        int x = findStart(map);
+
+        while (canTraverse(x, y, currentDirection, map))
+        {
+            var currentIcon = getMapIcon(x, y, map);
+
+            switch (currentIcon)
+            {
+                case traverseBlank:
+                    throw new InvalidOperationException();
+                case traverseTurn:
+                    currentDirection = getNewDirection(x, y, currentDirection, map);
+                    break;
+                case traverseNS:
+                case traverseEW:
+                    // ignore
+                    break;
+                default:
+                    letters.Append(currentIcon);
+                    break;
+            }
+
+            x = getX(x, currentDirection);
+            y = getY(y, currentDirection);
+        }
+
+        return letters.ToString();
+    }
+
+    private bool canTraverse(
+        int x,
+        int y,
+        Direction currentDirection,
+        IList<string> map)
+    {
+        return getMapIcon(x, y, map) != traverseBlank;
+    }
+
+    private int findStart(IList<string> map) =>
+        map.First().IndexOf(traverseNS);
+
+    private Direction getNewDirection(
+        int x,
+        int y,
+        Direction currentDirection,
+        IList<string> map)
+    {
+        foreach (var direction in getChangingDirectionOptions(currentDirection))
+        {
+            var newX = getX(x, direction);
+            var newY = getY(y, direction);
+            var newIcon = getMapIcon(newX, newY, map);
+
+            if (newIcon != traverseBlank)
+            {
+                return direction;
+            }
+        }
+
+        throw new InvalidOperationException("Cannot use any direction options");
+    }
+
+    private IEnumerable<Direction> getChangingDirectionOptions(Direction direction)
+    {
+        switch (direction)
+        {
+            case Direction.North:
+            case Direction.South:
+                return EastWest;
+            case Direction.West:
+            case Direction.East:
+                return NorthSouth;
+        }
+
+        throw new ArgumentException(nameof(direction));
+    }
+
+    private int getX(int x, Direction direction) =>
+        getPosition(x, direction, Direction.West, Direction.East);
+
+    private int getY(int y, Direction direction) =>
+        getPosition(y, direction, Direction.North, Direction.South);
+
+    private int getPosition(
+        int pos,
+        Direction toGo,
+        Direction subtractor,
+        Direction adder)
+    {
+        if (toGo == subtractor)
+        {
+            return pos - 1;
+        }
+
+        if (toGo == adder)
+        {
+            return pos + 1;
+        }
+
+        return pos;
+    }
+
+    private char getMapIcon(int x, int y, IList<string> map) => map[y][x];
+
+    private enum Direction
+    {
+        North,
+        South,
+        East,
+        West
+    }
+}
