2b41da6923f7630383db99f01eda6e0a615a2053
Bret Gourdie <BretGourdie@gmail.com>
2023-07-20 17:48:31 -0400

    Still implementing; to fix addSlice()

---
+++ advent-of-code-2017/Day21/Day21.cs
@@ -0,0 +1,149 @@
+﻿using System.Data;
+
+namespace advent_of_code_2017.Day21;
+internal class Day21 : AdventSolution
+{
+    private const char pixelTurnedOn = '#';
+
+    private readonly char[,] startingShape = new[,]
+    {
+        {'.', '#', '.'}, 
+        {'.', '.', '#'},
+        {'#', '#', '#'}
+    };
+
+    protected override long part1Work(string[] input)
+    {
+        var shape = startingShape;
+
+        var rules = generateRules(input);
+
+        var twosRules = rules.Where(x => x.Type == Rule.RuleType.TwosRule).ToList();
+        var threesRules = rules.Where(x => x.Type == Rule.RuleType.ThreesRule).ToList();
+
+        for (int ii = 0; ii < 5; ii++)
+        {
+            var size = shape.GetLength(0);
+
+            if (size % 2 == 0)
+            {
+                shape = transformByTwos(shape, twosRules);
+            }
+
+            else if (size % 3 == 0)
+            {
+                shape = transformByThrees(shape, threesRules);
+            }
+        }
+
+        return pixelCount(shape, pixelTurnedOn);
+    }
+
+    private long pixelCount(
+        char[,] shape,
+        char pixel)
+    {
+        long total = 0;
+
+        for (int ii = 0; ii < shape.GetLength(0); ii++)
+        {
+            for (int jj = 0; jj < shape.GetLength(1); jj++)
+            {
+                if (shape[ii,jj] == pixel)
+                {
+                    total += 1;
+                }
+            }
+        }
+
+        return total;
+    }
+
+    private IList<Rule> generateRules(IList<string> input)
+    {
+        var rules = new List<Rule>();
+        foreach (var line in input)
+        {
+            rules.Add(new Rule(line));
+        }
+
+        return rules;
+    }
+
+    private char[,] transformByTwos(
+        char[,] shape,
+        IList<Rule> twosRules) => transform(shape, twosRules, 2);
+
+    private char[,] transformByThrees(
+        char[,] shape,
+        IList<Rule> threesRules) => transform(shape, threesRules, 3);
+
+    private char[,] transform(
+        char[,] shape,
+        IList<Rule> rules,
+        int subdivisionLength)
+    {
+        var yLen = shape.GetLength(0);
+        var xLen = shape.GetLength(1);
+
+        char[,] newShape = new char[yLen + 1, xLen + 1];
+
+        for (int y = 0; y < shape.GetLength(0) / subdivisionLength; y += subdivisionLength)
+        {
+            for (int x = 0; x < shape.GetLength(1) / subdivisionLength; x += subdivisionLength)
+            {
+                char[,] slice = getSlice(shape, x, y, subdivisionLength);
+                foreach (var rule in rules)
+                {
+                    if (rule.Qualifies(slice))
+                    {
+                        slice = rule.Output;
+                    }
+                }
+
+                throw new NotImplementedException("have to adjust for x/y offset in new shape");
+                addSlice(slice, newShape, x, y);
+            }
+        }
+
+        return newShape;
+    }
+
+    private void addSlice(
+        char[,] slice,
+        char[,] target,
+        int x,
+        int y)
+    {
+        throw new NotImplementedException();
+    }
+
+    private char[,] getSlice(
+        char[,] shape,
+        int x,
+        int y,
+        int length)
+    {
+        var slice = new char[length, length];
+
+        for (int ii = y; ii < y + length; ii++)
+        {
+            for (int jj = x; jj < x + length; jj++)
+            {
+                slice[ii - y, jj - x] = shape[y, x];
+            }
+        }
+
+        return slice;
+    }
+
+    protected override long part1ExampleExpected => 12;
+    protected override long part1InputExpected => -1;
+    protected override long part2Work(string[] input)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override long part2ExampleExpected { get; }
+    protected override long part2InputExpected { get; }
+}
---
+++ advent-of-code-2017/Day21/Rule.cs
@@ -0,0 +1,151 @@
+﻿namespace advent_of_code_2017.Day21;
+
+internal class Rule
+{
+    public readonly char[,] Input;
+    public readonly char[,] Output;
+
+    private static readonly Rotation[] rotations = 
+    {
+        Rotation.None,
+        Rotation.Clockwise90,
+        Rotation.Clockwise180,
+        Rotation.Clockwise270
+    };
+
+    private static readonly Reflection[] reflections =
+    {
+        Reflection.None,
+        Reflection.Horizontal
+    };
+
+    private readonly IList<char[,]> permutations;
+
+    public readonly RuleType Type;
+
+    public Rule(string line)
+    {
+        var split = line.Split(" => ");
+
+        Input = convertTo2DArray(split[0].Split('/'));
+        Output = convertTo2DArray(split[1].Split('/'));
+
+        Type = Input.GetLength(0) == 2 ? RuleType.TwosRule : RuleType.ThreesRule;
+
+        permutations = generatePermutations(Input);
+    }
+
+    private char[,] convertTo2DArray(IList<string> input)
+    {
+        var grid = new char[input.Count, input[0].Length];
+
+        for (int y = 0; y < grid.Length; y++)
+        {
+            for (int x = 0; x < input.Count; x++)
+            {
+                grid[y, x] = input[y][x];
+            }
+        }
+
+        return grid;
+    }
+
+    public bool Qualifies(char[,] segment) =>
+        permutations.Any(x => matches(segment, x));
+
+    private IList<char[,]> generatePermutations(char[,] initial)
+    {
+        var permutations = new List<char[,]>();
+        foreach (var reflection in reflections)
+        {
+
+            foreach (var rotation in rotations)
+            {
+                var permuted = permute(initial, rotation, reflection);
+                permutations.Add(permuted);
+            }
+        }
+
+        return permutations;
+    }
+
+    private char[,] permute(
+        char[,] initial,
+        Rotation rotation,
+        Reflection reflection)
+    {
+        var yLen = initial.GetLength(0);
+        var xLen = initial.GetLength(1);
+
+        var numberOfRotations = Array.IndexOf(rotations, rotation);
+
+        char[,] result = initial;
+        char[,] toBeTransformed = initial;
+
+        for (int currentRotation = 0; currentRotation < numberOfRotations; currentRotation++)
+        {
+            result = new char[yLen, xLen];
+
+            for (int y = 0; y < yLen; y++)
+            {
+                for (int x = 0; x < xLen; x++)
+                {
+                    result[x, y] = toBeTransformed[y, x];
+                }
+            }
+
+            for (int y = 0; y < yLen; y++)
+            {
+                for (int x = 0; x < xLen; x++)
+                {
+                    result[y, x] = toBeTransformed[yLen - y - 1, xLen - x - 1];
+                }
+            }
+
+            toBeTransformed = result;
+        }
+
+        if (reflection == Reflection.Horizontal)
+        {
+
+        }
+
+        return result;
+    }
+
+    private bool matches(char[,] segment, char[,] permutation)
+    {
+        for (int y = 0; y < segment.GetLength(0); y++)
+        {
+            for (int x = 0; x < segment.GetLength(1); x++)
+            {
+                if (segment[y, x] != permutation[y, x])
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public enum RuleType
+    {
+        TwosRule,
+        ThreesRule
+    }
+
+    private enum Rotation
+    {
+        None,
+        Clockwise90,
+        Clockwise180,
+        Clockwise270
+    }
+
+    private enum Reflection
+    {
+        None,
+        Horizontal
+    }
+}
