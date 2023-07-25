186d8690f476f626b73e3d890917981dcc2aad30
Bret Gourdie <BretGourdie@gmail.com>
2023-07-25 12:50:35 -0400

    Failed at solving Day23
    Need to optimize the original program

---
+++ advent-of-code-2017/Day23/ICoProcessorStrategy.cs
@@ -0,0 +1,7 @@
+﻿namespace advent_of_code_2017.Day23;
+internal interface ICoProcessorStrategy
+{
+    IDictionary<string, long> InitializeRegisters();
+    long GetAnswer(IDictionary<string, long> registers);
+    void HandleInstructionProcessed(string instruction);
+}
---
+++ advent-of-code-2017/Day23/Troubleshooting.cs
@@ -0,0 +1,21 @@
+﻿namespace advent_of_code_2017.Day23;
+internal class Troubleshooting : ICoProcessorStrategy
+{
+    public IDictionary<string, long> InitializeRegisters()
+    {
+        var registers = new Dictionary<string, long>();
+
+        for (int ii = 0; ii < 8; ii++)
+        {
+            registers[('a' + ii).ToString()] = 0;
+        }
+
+        registers["a"] = 1;
+
+        return registers;
+    }
+
+    public long GetAnswer(IDictionary<string, long> registers) => registers["h"];
+
+    public void HandleInstructionProcessed(string instruction) { }
+}
