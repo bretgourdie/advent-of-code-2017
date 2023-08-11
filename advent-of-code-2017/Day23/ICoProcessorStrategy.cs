88394b4bb67716f46cd81aac962d9e5f672c8f8f
Bret Gourdie <BretGourdie@gmail.com>
2023-08-11 17:20:49 -0400

    Solve Day 23 Part 2

--- advent-of-code-2017/Day23/ICoProcessorStrategy.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace advent_of_code_2017.Day23;
-internal interface ICoProcessorStrategy
-{
-    IDictionary<string, long> InitializeRegisters();
-    long GetAnswer(IDictionary<string, long> registers);
-    void HandleInstructionProcessed(string instruction);
-}
